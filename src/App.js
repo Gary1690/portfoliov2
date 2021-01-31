dd120649b7ac34bd7e76452da7bd35b0f26dd954
Gary Cordero <GaryCordero1690@gmail.com>
2021-01-31 00:05:46 -0500

    layout completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import './App.css';
+import './App.scss';
+import Layout from './components/layout/layout.js';
+import {Navbar, Nav} from 'react-bootstrap';
+import {Link,Element} from 'react-scroll';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello from app</h1>
-    </div>
+    <>
+    <Navbar  bg="dark" variant="dark" expand="md"  sticky="top" style={{opacity:"0.8",zIndex:"10"}}>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto justify-content-center text-light" style={{ width: "100%" }}>
+         <Nav.Item className="m-1">
+          <Link activeClass="active" to="test1" spy={true} smooth={true} offset={-50} duration={1000} >
+            Test 1
+          </Link>
+         </Nav.Item>
+         <Nav.Item className="m-1"> 
+          <Link activeClass="active" to="test2" spy={true} smooth={true} offset={50} duration={1000} >
+            Test 2
+          </Link>
+         </Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+    <section className="pt-5" id="test1" style={{height:"100vh",backgroundColor:"blue"}}>
+      <h1>Section 1</h1>
+    </section>
+    <section id="test2" style={{height:"100vh",backgroundColor:"red"}}>
+      <h1>Section 1</h1>
+    </section>
+
+    <footer className="footer">
+      <p>
+        Author: Hege Refsnes <a href="mailto:hege@example.com">hege@example.com</a>
+      </p>
+    </footer>
+    </>
+
+    
   );
 }
 
---
+++ src/components/layout/layout.js
@@ -0,0 +1,22 @@
+// import {Navbar,Nav,NavDropdown,Container} from 'react-bootstrap';
+import { Link } from 'react-scroll'
+
+
+
+const Layout = ( ) => {
+
+}
+
+const Footer = () =>{
+
+
+}
+
+const Header = () =>{
+
+  return(
+    <></>
+  )
+}
+
+export default Layout ;
