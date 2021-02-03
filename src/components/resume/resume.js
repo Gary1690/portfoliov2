85742b3d4d7019a9705cb7bf4f6e53de7dab0cec
Gary Cordero <GaryCordero1690@gmail.com>
2021-02-03 14:21:19 -0500

    making resume section responsive

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -1,54 +1,59 @@
 import React from 'react'
 import {Row,Col} from 'react-bootstrap'
+import './resume.scss'
 
 function Resume() {
   return (
     <div id="resume" className="section resume">
-      <div className="center" >
-        <Row className="w-100 mt-5">
-          <Col sm={12}  md={{span:2,offset:2}} className="resume-left">
-              <h3 className="resume-title"><span>EDUCATION</span></h3>
+      <div className="center">
+      <Row className="w-100 mt-5">
+          <Col xs={12} md={12} lg={4} className="text-center">
+            <h3 className="resume-title"><span>EDUCATION</span></h3>
           </Col>
-          <Col sm={12} md={8} className="d-flex flex-column">
+          <Col  xs={12} md={12} lg={8} className="text-center">
+            <div className="text-lg-left px-5">
               <h3 className="resume-title">Flatiron School</h3>
-              <div className="description">
-                <p className="achievement">Software Engineering Inmersive</p>
-                <p className="date"> Aug. 2020 </p>
-              </div>
-              <br/>
-              <h3 className="resume-title">Universidad Autonoma de Santo Domingo</h3>
-              <div className="description">
-                <p className="achievement">B.S. in Computer Science</p>
-                <p className="date"> Oct. 2017 </p>
-              </div>
+            </div>
+            <div className="description px-5">
+              <p className="achievement">Software Engineering Inmersive</p>
+              <p className="date"> Aug. 2020 </p>
+            </div>
+            <br/>
+            <div className="text-lg-left px-5">
+             <h3 className="resume-title">Universidad Autonoma de Santo Domingo</h3>
+            </div>
+            <div className="description px-5">
+              <p className="achievement">B.S. in Computer Science</p>
+              <p className="date"> Oct. 2017 </p>
+            </div>
           </Col>
         </Row>
-        <hr className="divisor w-75"/>
-        <Row  className="w-100 mt-5" >
-          <Col sm={12}  md={{span:2,offset:2}} className="resume-left">
-              <h3 className="resume-title"><span>WORK EXPERIENCE</span></h3>
+        <Row className="w-100 mt-5">
+          <Col xs={12} md={12} lg={4} className="text-center">
+            <h3 className="resume-title"><span>WORK EXPERIENCE</span></h3>
           </Col>
-          <Col sm={12} md={8} className="d-flex flex-column align-items-start" >
-              <div className="description">
+          <Col  xs={12} md={12} lg={8} className="text-center">
+          <div className="description px-5">
               <h3 className="resume-title">Away</h3>
               <p className="location" > New York, NY</p> 
               </div>
-              <div className="description">
+              <div className="description px-5">
                 <p className="achievement"> Sales Associate </p> 
                 <p className="date"> May 2018 - Oct. 2019</p> 
               </div>
               <br/>
-              <div className="description">
+              <div className="description px-5">
                 <h3 className="resume-title">Charles Tyrwhitt</h3>
                 <p className="location" > New York, NY</p> 
               </div>
-              <div className="description">
+              <div className="description px-5">
                 <p className="achievement" >Key Holder </p> 
                 <p className="date" > May 2018 - Oct. 2019</p> 
               </div>
           </Col>
         </Row>
       </div>
+        
     </div>
   )
 }
