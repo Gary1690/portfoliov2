70a4845cb981ee5273ae316749d2c067e82cc611
Gary Cordero <GaryCordero1690@gmail.com>
2021-02-03 14:20:53 -0500

    making about section responsive

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,5 +1,7 @@
 import React, {useEffect} from 'react'
 import avatar from './gary.jpg'
+import {Col,Row} from 'react-bootstrap'
+import './about.scss'
 
 function About() {
 
@@ -9,21 +11,19 @@ function About() {
 
   return (
     <div id="about" className="section about" >
-      <div className="center">
-        <div className="d-flex flex-variant">
-          <div className = "text-center m-5 " >
-             <img
-              className= "avatar m-2"
-              src= {avatar}
-              alt="Gary Cordero"
-            />
-          </div>
-          <div className = "">
-            <h4 className="resume-title p-3"><span>About Me</span></h4>
-            <p className="message p-3"> Full stack web developer with strengths in Ruby, C#, Java and JavaScript frameworks and a strong foundation in software engineering and object oriented programming concepts with experience in retail sales and business operations. </p>
-          </div>
+        <div className="center">
+          <Row className="w-100">
+            <Col xs={12} md={12} lg={4}>
+              <div className="d-flex align-items-center w-100 h-100">
+                <img src={avatar} className="avatar" alt="gary" />
+              </div>
+            </Col>
+            <Col  xs={12} md={12} lg={8}>
+              <h1 className="about-title"><span>About</span></h1>
+              <p className="about-message">Full-stack web developer with strengths in Ruby, Java, and JavaScript frameworks. Strong foundation in software engineering and object-oriented programming concepts with experience in retail sales and business operations. </p>
+            </Col>
+          </Row>
         </div>
-      </div>
     </div>
   )
 }
